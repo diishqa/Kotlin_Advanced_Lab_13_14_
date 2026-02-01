febe53a6e3c482c579e56712bcc296e75576b3e3
diishqa <diana.34@list.ru>
2026-02-01 14:22:42 +0300

    Arrays in Kotlin

--- src/Main.kt
+++ src/Main.kt
@@ -61,7 +61,28 @@ class Quiz : ProgressPrintable {
 
 fun main() {
 //    Quiz().printProgressBar()
-    Quiz().apply { printQuiz() }
+//    Quiz().apply { printQuiz() }
+    println(solarSystem[0])
+    println(solarSystem[1])
+    println(solarSystem[2])
+    println(solarSystem[3])
+    println(solarSystem[4])
+    println(solarSystem[5])
+    println(solarSystem[6])
+    println(solarSystem[7])
+    solarSystem[3]="Little Earth"
+    println(solarSystem[3])
+    val newSolarSystem = arrayOf(
+        "Mercury",
+        "Venus",
+        "Earth",
+        "Mars",
+        "Jupiter",
+        "Saturn",
+        "Uranus",
+        "Neptune",
+        "Pluto"
+    )
 
 
 
