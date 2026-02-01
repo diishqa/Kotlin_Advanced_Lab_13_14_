e4b6c4432094cccc3027762486843e7b65cfe269
diishqa <diana.34@list.ru>
2026-02-01 14:12:07 +0300

    Rewrite extensions using interface

--- src/Main.kt
+++ src/Main.kt
@@ -1,4 +1,19 @@
-class Quiz {
+interface ProgressPrintable {
+    val progressText: String
+    fun printProgressBar()
+}
+
+class Quiz : ProgressPrintable {
+    override val progressText: String
+        get() = "${aswered} of ${total} answered"
+
+    override fun printProgressBar() {
+        repeat(Quiz.aswered) { kotlin.io.print("▓") }
+        repeat(Quiz.total - Quiz.aswered) { print("▓") }
+        println()
+        println(progressText)
+    }
+
     val question1 = Question<String>(
         "Речка спятила с ума - По домам пошла сама. ___",
         "Водопровод",
@@ -14,24 +29,15 @@ class Quiz {
         28,
         Difficulty.HARD
     )
+
     companion object StudentProgress {
         var total: Int=10
         var aswered: Int=3
     }
 }
 
-val Quiz.StudentProgress.progressText: String
-    get()="${aswered} of ${total} answered"
-
-fun Quiz.StudentProgress.printProgressBar() {
-    repeat(Quiz.aswered) { print("▓") }
-    repeat(Quiz.total - Quiz.aswered) { print("▓") }
-    println()
-    println(Quiz.progressText)
-}
-
 fun main() {
-    Quiz.printProgressBar()
+    Quiz().printProgressBar()
 
 
 
