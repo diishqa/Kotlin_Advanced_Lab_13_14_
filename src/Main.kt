aedd11f0f5bdbd61cfa64ab32320e4c50ae9c0d7
diishqa <diana.34@list.ru>
2026-02-01 14:28:35 +0300

    Lists in Kotlin

--- src/Main.kt
+++ src/Main.kt
@@ -62,27 +62,45 @@ class Quiz : ProgressPrintable {
 fun main() {
 //    Quiz().printProgressBar()
 //    Quiz().apply { printQuiz() }
-    println(solarSystem[0])
-    println(solarSystem[1])
-    println(solarSystem[2])
+//    println(solarSystem[0])
+//    println(solarSystem[1])
+//    println(solarSystem[2])
+//    println(solarSystem[3])
+//    println(solarSystem[4])
+//    println(solarSystem[5])
+//    println(solarSystem[6])
+//    println(solarSystem[7])
+//    solarSystem[3]="Little Earth"
+//    println(solarSystem[3])
+//    val newSolarSystem = arrayOf(
+//        "Mercury",
+//        "Venus",
+//        "Earth",
+//        "Mars",
+//        "Jupiter",
+//        "Saturn",
+//        "Uranus",
+//        "Neptune",
+//        "Pluto"
+//    )
+
+    val solarSystem= mutableListOf("Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune")
+    solarSystem.add("Pluto")
+    solarSystem.add(3, "Theia")
+    solarSystem[3]="Future Moon"
     println(solarSystem[3])
-    println(solarSystem[4])
-    println(solarSystem[5])
-    println(solarSystem[6])
-    println(solarSystem[7])
-    solarSystem[3]="Little Earth"
-    println(solarSystem[3])
-    val newSolarSystem = arrayOf(
-        "Mercury",
-        "Venus",
-        "Earth",
-        "Mars",
-        "Jupiter",
-        "Saturn",
-        "Uranus",
-        "Neptune",
-        "Pluto"
-    )
+    println(solarSystem[9])
+    solarSystem.removeAt(9)
+    println(solarSystem.contains("Pluto"))
+    println("Future Moon" in solarSystem)
+//    println(solarSystem.size)
+//    println(solarSystem[2])
+//    println(solarSystem.get(3))
+//    println(solarSystem.indexOf("Earth"))
+//    println(solarSystem.indexOf("Pluto"))
+//    for (planet in solarSystem){
+//        println(planet)
+//    }
 
 
 
